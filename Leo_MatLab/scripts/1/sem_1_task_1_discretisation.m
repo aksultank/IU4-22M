962c2d946f3fe8a105e7005ba218d12c10f2eb56
Chpeck <uzen.demid@gmail.com>
2020-02-26 16:10:12 +0300

    fix sem 1 task 1

--- Leo_MatLab/scripts/1/sem_1_task_1_discretisation.m
+++ Leo_MatLab/scripts/1/sem_1_task_1_discretisation.m
@@ -1,10 +1,11 @@
 clear;
 
 xy_grid_max_1 = 20; % grid number settings
-xy_grid_max_2 = 30;
-xy_grid_max_3 = 70;
+xy_grid_max_2 = 240;
+xy_grid_max_3 = 500;
 
-fs0 = 120; % discretisation freq
+fs_base = 10;
+fs0 = 15; % discretisation freq
 fs1 = 180;
 fs2 = 360;
 
@@ -14,9 +15,9 @@ ts0  = 0:1/fs0:T; % array of signal time points
 ts1  = 0:1/fs1:T;
 ts2  = 0:1/fs2:T;
 
-y = sin(2*pi*10*ts0); % array of signal amp points
-y1 = 2*sin(2*pi*10*ts1);
-y2 = 1/2*(sin(2*pi*10*ts2));
+y = sin(2*pi*fs_base*ts0); % array of signal amp points
+y1 = 2*sin(2*pi*fs_base*ts1);
+y2 = 1/2*(sin(2*pi*fs_base*ts2));
 
 plot(ts0(1:xy_grid_max_1),y(1:xy_grid_max_1),'-black+'...
 ,ts1(1:xy_grid_max_2),y1(1:xy_grid_max_2),'-red^'...
